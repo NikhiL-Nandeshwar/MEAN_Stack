b19cf61bbae8559c0d69ade27ac2c4ae7e6e075e
nik580 <580@users.noreply.github.com>
2021-03-29 20:29:17 +0100

    init

---
+++ Backend/Database/models/task.js
@@ -0,0 +1,27 @@
+const mongoose = require ('mongoose');
+
+const TaskSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            trim: true,
+            minlength: 3
+        },
+        _listId:
+        {
+            type: mongoose.Types.ObjectId,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            dafault: false,
+            required: true
+    
+        }
+
+    });
+
+    const Task = mongoose.model('Task',TaskSchema);
+    module.exports = Task;
+
+    
---
+++ Backend/Database/mongoose.js
@@ -0,0 +1,10 @@
+ const mongoose = require('mongoose');
+ mongoose.Promise = global.Promise;
+
+ mongoose.connect('mongodb://127.0.0.1:27017/taskmanager',{ useNewUrlParser: true ,useUnifiedTopology: true,
+ useFindAndModify: false } )
+ .then(() =>console.log("database is connected")).catch((error) => console.log(error)); 
+ 
+
+
+ module.exports = mongoose;
